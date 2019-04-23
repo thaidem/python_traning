b855c61920f73958760115138f9270d995cc53d3
thaidem <49270614+thaidem@users.noreply.github.com>
2019-04-23 16:40:39 +0300

    Логин и логаут - предусловия

--- test/test_app_g1004.py
+++ test/test_app_g1004.py
@@ -4,6 +4,7 @@ from model.group import Group
 
 def test_app_group(app):
     app.group.create(Group(name="ffgnghvnb", header="dgdffbxf", footer="dVSxvbcb"))
+#    app.session.logout()
 
 
 def test_app_emptygroup(app):
