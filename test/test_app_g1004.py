9cd33ac9c9e01a087306af93b10c8c02fd5666b5
thaidem <49270614+thaidem@users.noreply.github.com>
2019-04-23 11:29:56 +0300

    Интеллектуальная фикстура

--- test/test_app_g1004.py
+++ test/test_app_g1004.py
@@ -3,12 +3,8 @@ from model.group import Group
 
 
 def test_app_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="ffgnghvnb", header="dgdffbxf", footer="dVSxvbcb"))
-    app.session.logout()
 
 
 def test_app_emptygroup(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
