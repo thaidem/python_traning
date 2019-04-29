eb095e1a7bc6d36ff6a023e384b4f72f93f2a5bf
thaidem <49270614+thaidem@users.noreply.github.com>
2019-04-29 15:50:05 +0300

    сравнение и сортировка групп

--- test/test_app_g1004.py
+++ test/test_app_g1004.py
@@ -4,13 +4,19 @@ from model.group import Group
 
 def test_app_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="ffgnghvnb", header="dgdffbxf", footer="dVSxvbcb"))
+    group = Group(name="ffgnghvnb", header="dgdffbxf", footer="dVSxvbcb")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_app_emptygroup(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="", header="", footer=""))
+    group = Group(name="", header="", footer="")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
